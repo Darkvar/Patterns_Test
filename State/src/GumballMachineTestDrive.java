7dcadff83612b74c944fc39df251e84c7142cf7c
darkvar <ispaniol@te.net.ua>
2013-07-09 16:52:51 +0300

    State Pattern Example Implementation First version of Bum ball machine, needs to be refactored

---
+++ State/src/GumballMachineTestDrive.java
@@ -0,0 +1,42 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 09.07.13
+ *
+ * @author: mkalachyov
+ */
+public class GumballMachineTestDrive {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+}
