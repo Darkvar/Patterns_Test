0bd7708a4ef837837f7a19282fa85511e4696521
darkvar <ispaniol@te.net.ua>
2013-07-26 16:39:19 +0300

    Patterns Composition Example Implementation Duck first commit, skeleton

---
+++ Composite_Patterns/src/Quackable.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 26.07.13
+ *
+ * @author: mkalachyov
+ */
+public interface Quackable {
+    public void quack();
+}
---
+++ Composite_Patterns/src/RubberDuck.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 26.07.13
+ *
+ * @author: mkalachyov
+ */
+public class RubberDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
