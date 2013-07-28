e94f55619c2bd3c04194e5e9003d7773b1f8207d
darkvar <ispaniol@te.net.ua>
2013-07-28 14:11:39 +0300

    Patterns Composition Example Implementation Added Abstract Factory Pattern implementation

---
+++ Composite_Patterns/src/AbstractDuckFactory.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 28.07.13
+ *
+ * @author: mkalachyov
+ */
+public abstract class AbstractDuckFactory {
+    public abstract Quackable createMallardDuck();
+    public abstract Quackable createRedHeadDuck();
+    public abstract Quackable createDuckCall();
+    public abstract Quackable createRubberDuck();
+}
---
+++ Composite_Patterns/src/CountingDuckFactory.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 28.07.13
+ *
+ * @author: mkalachyov
+ */
+public class CountingDuckFactory extends AbstractDuckFactory {
+    @Override
+    public Quackable createMallardDuck() {
+        return new QuackCounter(new MallardDuck());
+    }
+
+    @Override
+    public Quackable createRedHeadDuck() {
+        return new QuackCounter(new RedHeadDuck());
+    }
+
+    @Override
+    public Quackable createDuckCall() {
+        return new QuackCounter(new DuckCall());
+    }
+
+    @Override
+    public Quackable createRubberDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+}
